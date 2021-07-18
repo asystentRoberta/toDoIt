a39ad4b1dd84641cb92bb5038af32271832588dc
AsystentRoberta <asystent.roberta@gmail.com>
2021-07-18 17:54:10 +0200

    Remove SpringBootTest annotation

--- src/test/java/pl/com/bohdziewicz/todoit/ToDoItApplicationTests.java
+++ src/test/java/pl/com/bohdziewicz/todoit/ToDoItApplicationTests.java
@@ -1,11 +1,13 @@
 package pl.com.bohdziewicz.todoit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ToDoItApplicationTests {
 
-    @Test
-    void contextLoads() {
+	@Test
+	void contextLoads() {
+	}
 
-    }
 }
